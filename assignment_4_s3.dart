c426a4b2d1ff3a10bf7d73e70cc2f1a27653a8b4
tryggos16 <tryggvi.bogason@gmail.com>
2022-03-11 23:20:30 +0000

    dagur 1-2

--- assignment_4_s3.dart
+++ assignment_4_s3.dart
@@ -1,23 +1,35 @@
-
 import "dart:io";
+
 
 void main() {
   List<int> numberStore = [];
-  print("enter a number:");
-
   bool isStarted = false;
+  print("enter a number");
 
   while(true) {
     String input = stdin.readLineSync();
-    numberStore.add(input);
-    numberStore.sort();
-    isStarted = true;
 
-    if(input?.isEmpty ?? true)
+    if(input.contains(RegExp(r'[0-9]') )) {
+      numberStore.add(int.parse(input));
+      numberStore.sort();
+      print(numberStore);
+      isStarted = true;
 
+    }else if(input.contains(RegExp(r'[a-z]'))) {
+      print("not a number!");
 
+    }else if(isStarted == true && input.isEmpty == true) {
+      print("Outcome");
+      print("----------");
+      print("min " + numberStore[0].toString());
+      print("max " + numberStore[numberStore.length -1].toString());
+      print("----------");
 
+    }else {
+      print("Enter a number first!");
 
+    }
 
+  }
 
 }
