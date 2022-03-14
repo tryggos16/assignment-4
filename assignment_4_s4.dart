2bc651f41243c499d13154c6f76ccc978ce0ecfe
tryggos16 <tryggvi.bogason@gmail.com>
2022-03-14 00:12:29 +0000

    dagur 2-2

--- assignment_4_s4.dart
+++ assignment_4_s4.dart
@@ -3,26 +3,25 @@ import "dart:io";
 void main() {
   List<String> listOfStrings = [];
   bool isStarted = false;
+  print("enter a num or letter:");
 
   while(true) {
     String input = stdin.readLineSync();
 
-    if(input.contains(RegExp(r'[a-z]'))) {
+    if(input.contains(RegExp(r'[a-z]')) || input.contains(RegExp(r'[0-9]'))) {
       listOfStrings.add(input);
       print(listOfStrings);
       isStarted = true;
-
-    }else if(input.contains(RegExp(r'[0-9]'))) {
-      print("this is a int!");
 
     }else if(isStarted == true && input.isEmpty == true) {
       print("----------");
       print(listOfStrings);
       print(listOfStrings.reversed);
       print("----------");
+      break;
 
     }else {
-      print("you must give a !");
+      print("you must give an input!");
     }
 
   }
